fd091f13fc073ce58c3462078cd1c75d2f0fa5a2
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2022-06-21 22:50:17 +0100

    add moving background divs

--- src/App.js
+++ src/App.js
@@ -16,12 +16,28 @@ function App() {
       </header>
       <div className="main">
         <div className="intro">
-          <div>
+          <div className="introText">
             <h2>Hi there, I'm</h2>
             <h1>Layla Southcombe</h1>
             <h2>Full-Stack Developer</h2>
           </div>
-          <img id="memoji" src={memoji} alt="Layla Southcombe Memoji" />
+          <div className="memoji">
+            <img id="memoji" src={memoji} alt="Layla Southcombe Memoji" />
+          </div>
+          <ul class='circles'>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+          <div className="squares">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
         </div>
           <About id="about"/>
           <Skills id="skills"/>

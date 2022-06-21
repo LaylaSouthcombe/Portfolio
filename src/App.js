98a9385fea297710483dc86319ecbfc18db6ccf9
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2022-06-21 08:56:59 +0100

    add styling to nav section

--- src/App.js
+++ src/App.js
@@ -2,14 +2,27 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { About, Contact, Projects, Skills } from './pages'
 import { NavBar } from './layout'
+import memoji from './images/Memoji.png'
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <header>
+        <img id="logo" width="160px" height="160px"></img>
+        <NavBar />
+        <button id="resume">RESUME</button>
+      </header>
       <div className="main">
+        <div className="intro">
+          <div>
+            <h2>Hi there, I'm</h2>
+            <h1>Layla Southcombe</h1>
+            <h2>Full-Stack Developer</h2>
+          </div>
+          <img id="memoji" src={memoji} alt="Layla Southcombe Memoji" />
+        </div>
           <About id="about"/>
           <Skills id="skills"/>
           <Projects id="projects"/>
--- src/layout/NavBar/index.js
+++ src/layout/NavBar/index.js
@@ -3,16 +3,16 @@ import { Link } from "react-scroll";
 
 const NavBar = () => {
     return(
-    <div className="nav">
-        <nav className="navBar">
-            <ul>
-                <li><Link activeClass="active" smooth spy to="about">About</Link></li>
-                <li><Link activeClass="active" smooth spy to="skills">Skills</Link></li>
-                <li><Link activeClass="active" smooth spy to="projects">Projects</Link></li>
-                <li><Link activeClass="active" smooth spy to="contact">Contact Me</Link></li>
+
+        <nav >
+            <ul className="navBar">
+                <li><Link activeClass="active" smooth spy to="about"><span className="number">01.</span> About</Link></li>
+                <li><Link activeClass="active" smooth spy to="skills"><span className="number">02.</span> Skills</Link></li>
+                <li><Link activeClass="active" smooth spy to="projects"><span className="number">03.</span> Projects</Link></li>
+                <li><Link activeClass="active" smooth spy to="contact"><span className="number">04.</span> Contact Me</Link></li>
             </ul>
         </nav>
-    </div>
+
     )
 }
 

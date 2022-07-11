c84abed49d428b655620fdad9345aeb5b582a122
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2022-07-11 13:12:17 +0100

    add Google Analytics tag

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import { About, Contact, Projects, Skills } from './pages'
 import { NavBar } from './layout'
 import memoji from './images/Memoji.png'

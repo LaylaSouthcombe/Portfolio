37bfff84099c60ecaf8a8d8c4d54fb8de5ec2b5d
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2023-05-26 16:15:41 +0100

    add netlify attribute

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -8,11 +8,12 @@ const Contact = ({zIndex, innerRef}) => {
                 <h2><span className="number">04.</span> Get In Touch</h2>
                 <div className="lineType3 line"></div>
             </div> 
-            <form method="POST" encType="text/plain" className="contactForm" autoComplete="off" name="contact" data-netlify="true">
-                <input type="text" name="fullName" id="fullName" placeholder="Full name" className="formItem1" style={{zIndex: zIndex}}  minLength="5" required/>
-                <input type="email" name="email" id="email" placeholder="Email address" className="formItem2" style={{zIndex: zIndex}}  minLength="10" required/>
-                <input type="number" name="phoneNum" id="phoneNum" placeholder="Phone number" className="formItem3" style={{zIndex: zIndex}} minLength="11" required/>
-                <textarea name="message" id="message" cols="30" rows="10" placeholder="Message" className="formItem4" spellCheck="true" style={{zIndex: zIndex}} minLength="50" required></textarea>
+            {/* <form action="mailto:laylasouthcombe789@outlook.com" method="post" encType="text/plain" className="contactForm" autoComplete="off"> */}
+            <form method="POST" encType="text/plain" className="contactForm" autoComplete="off" name="contactForm" data-netlify="true" netlify>
+                <input type="text" name="fullName" id="fullName" placeholder="Full name" className="formItem1" style={{zIndex: zIndex}} required/>
+                <input type="email" name="email" id="email" placeholder="Email address" className="formItem2" style={{zIndex: zIndex}} required/>
+                <input type="number" name="phoneNum" id="phoneNum" placeholder="Phone number" className="formItem3" style={{zIndex: zIndex}}/>
+                <textarea name="message" id="message" cols="30" rows="10" placeholder="Message" className="formItem4" spellCheck="true" style={{zIndex: zIndex}} minLength="5" required></textarea>
                 <input type="submit" className="sendEmail formItem5" value="SEND" />
             </form>
        </div>   

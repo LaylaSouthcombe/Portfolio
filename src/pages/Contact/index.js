c8dda111e7dc023c22baf420b35088813f63e470
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2023-05-26 16:57:47 +0100

    add in custom code

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -34,32 +34,14 @@ const Contact = ({zIndex, innerRef}) => {
                 <textarea name="message" id="message" cols="30" rows="10" placeholder="Message" className="formItem4" spellCheck="true" style={{zIndex: zIndex}} minLength="5" required></textarea>
                 <input type="submit" className="sendEmail formItem5" value="SEND" />
             </form> */}
-     <form 
-  method='POST' 
-  name='contactform' 
-  className='contactForm'>
-
-  <input 
-    type='hidden'
-    name='form-name'
-    value='contactForm' />
-
-  <input 
-    type='text' 
-    name='name' 
-    placeholder='Enter your name' />
-
-  <input 
-    type='email' 
-    name='email' 
-    placeholder='Enter your email' />
-
-  <textarea 
-    name='message' 
-    placeholder='Messaage'></textarea>
-
-  <button type='submit'>Submit</button>
-</form>
+            <form method='POST' name='contactform' className='contactForm'>
+                <input type='hidden' name='form-name' value='contactForm' />
+                <input type="text" name="fullName" id="fullName" placeholder="Full name" className="formItem1" style={{zIndex: zIndex}} required/>
+                <input type="email" name="email" id="email" placeholder="Email address" className="formItem2" style={{zIndex: zIndex}} required/>
+                <input type="number" name="phoneNum" id="phoneNum" placeholder="Phone number" className="formItem3" style={{zIndex: zIndex}}/>
+                <textarea name="message" id="message" cols="30" rows="10" placeholder="Message" className="formItem4" spellCheck="true" style={{zIndex: zIndex}} minLength="5" required></textarea>
+                <input type="submit" className="sendEmail formItem5" value="SEND" />
+            </form>
        </div>   
     )
 }

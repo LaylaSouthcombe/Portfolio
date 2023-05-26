ac7c6f1cd17573849d8d8d319dc561599f7fccdd
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2023-05-26 16:21:43 +0100

    add hidden form

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -9,30 +9,13 @@ const Contact = ({zIndex, innerRef}) => {
                 <div className="lineType3 line"></div>
             </div> 
             {/* <form action="mailto:laylasouthcombe789@outlook.com" method="post" encType="text/plain" className="contactForm" autoComplete="off"> */}
-            {/* <form method="POST" encType="text/plain" className="contactForm" autoComplete="off" name="contactForm" data-netlify="true" netlify>
+            <form method="POST" encType="text/plain" className="contactForm" autoComplete="off" name="contactForm" data-netlify="true" netlify>
                 <input type="text" name="fullName" id="fullName" placeholder="Full name" className="formItem1" style={{zIndex: zIndex}} required/>
                 <input type="email" name="email" id="email" placeholder="Email address" className="formItem2" style={{zIndex: zIndex}} required/>
                 <input type="number" name="phoneNum" id="phoneNum" placeholder="Phone number" className="formItem3" style={{zIndex: zIndex}}/>
                 <textarea name="message" id="message" cols="30" rows="10" placeholder="Message" className="formItem4" spellCheck="true" style={{zIndex: zIndex}} minLength="5" required></textarea>
                 <input type="submit" className="sendEmail formItem5" value="SEND" />
-            </form> */}
-            <form method="post" name="Contact Form" netlify>
-                <p>
-                    <label for="name">Name:</label>
-                    <input type="text" name="name" id="name"/>
-                </p>
-                <p>
-                    <label for="email">Email:</label>
-                    <input type="email" name="email" id="email"/>
-                </p>
-                <p>
-                    <label for="comments">Comments:</label><br/>
-                    <textarea name="comments" id="comments"></textarea>
-                </p>
-                <p>
-                    <input type="submit"/>
-                </p>
-                </form>
+            </form>
        </div>   
     )
 }

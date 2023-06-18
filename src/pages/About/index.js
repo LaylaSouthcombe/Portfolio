f3b51363357d65b4c1ab55d9662c2731394705e5
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2023-06-18 16:30:55 +0100

    rewrite about me css

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -11,7 +11,8 @@ const About = ({innerRef}) => {
             </div>
             <div className="aboutInfo wrapper">
                 <div className="aboutText">
-                    <p>As a full-stack developer with a background in pharmacology and medical editing, I have a unique blend of scientific expertise and technical skills. My strong foundation in problem-solving and analytical thinking aided my successful career transition to pursue my passion for coding.</p>
+                    <p>As a full-stack developer with a background in pharmacology and medical editing, I have a unique blend of scientific expertise and technical skills. </p>
+                    <p>My strong foundation in problem-solving and analytical thinking aided my successful career transition to pursue my passion for coding.</p>
                     <p>Having completed a comprehensive full-stack developer bootcamp, I am well-versed in various front- and back-end programming languages, frameworks, and technologies.</p> 
                     <p>As I continue my journey through the tech industry, I am excited to take on roles in which I can contribute my enthusiasm, curiosity, strong work ethic, and ability to quickly adapt and learn.</p>
                 </div>

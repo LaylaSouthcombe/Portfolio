cd4b26629a475d7767c99a279f70200eaeec5bd9
LaylaSouthcombe <laylasouthcombe789@outlook.com>
2023-10-03 12:16:16 +0100

    update the projects

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import { Project } from '../../components'
 import { motion } from "framer-motion";
-import sooki from '../../images/sooki.webp'
+import sojoTravels from '../../images/sojoTravels.webp'
 import rune from '../../images/rune.png'
 import indee from '../../images/indee.webp'
 
 const Projects = ({innerRef}) => {
 
     const projects = [
-        { id: 1, appType: "eCommerce Site", projectName: "Rune", summary: "A Next.js full-stack app with a MongoDB database, authentification, and Stripe payment integration for an eCommerce Site", technologies: ["Next.js", "HTML", "CSS", "axios", "JavaScript", "MongoDB", "bcrypt", "Styled Components", "Stripe", "mui"], github: [{ linkFor: "Front-end", link: "https://github.com/LaylaSouthcombe/fashion-site-frontend"}], deploy: "https://main--rune-store.netlify.app/", img: rune},
-        { id: 2, appType: "Habit Tracker", projectName: "Indee", summary: "A React full-stack app with a MongoDB database that aims to help people live more independently through tracking habits which can be viewed by a carer/guardian also", technologies: ["HTML", "CSS", "JavaScript", "Node.js", "Express", "PostgreSQL", "Heroku", "Bcrypt", "Chart.js", "Docker"], github: [{ linkFor: "Back-end", link: "https://github.com/LaylaSouthcombe/Indee_backend"}, { linkFor: "Front-end", link: "https://github.com/LaylaSouthcombe/Indee_frontend"}], deploy: "https://indee.netlify.app/", img: indee},
-        { id: 3, appType: "Meal Planner Site", projectName: "Sooki", summary: "A React full-stack app with a Python backend and SQL database to generate a random meal plan that takes in your dietary requirements and budget, and can generate a shopping list for you (group project)", technologies: ["HTML", "CSS",  "React", "Redux", "axios", "JavaScript", "Jest", "Python", "PyMongo", "Django", "pytest", "PostgreSQL", "Bcrypt"], github: [{ linkFor: "Back-end", link: "https://github.com/LaylaSouthcombe/meal_planner_backend"}, { linkFor: "Front-end", link: "https://github.com/LaylaSouthcombe/meal_planner_frontend"}], deploy: "https://digerati-meal-planner.netlify.app/", img: sooki}
+        { id: 1, appType: "Travel Article Site", projectName: "Sojo Travels", summary: "A React full-stack app with a PosetgreSQL database that provides a wide collection of travel articles about destinations in Europe", technologies: ["React", "HTML", "CSS", "JavaScript", "Node.js", "Express", "PostgreSQL", "mui", "axios", "Jest", "React Testing Library"], github: [{ linkFor: "Back-end", link: "https://github.com/LaylaSouthcombe/travel-site-backend"}, { linkFor: "Front-end", link: "https://github.com/LaylaSouthcombe/travel-site-frontend"}], deploy: "https://sojo-travels.netlify.app/", img: sojoTravels},
+        { id: 2, appType: "eCommerce Site", projectName: "Rune", summary: "A Next.js full-stack app with a MongoDB database, authentification, and Stripe payment integration for an eCommerce Site", technologies: ["Next.js", "HTML", "CSS", "axios", "JavaScript", "MongoDB", "bcrypt", "Styled Components", "Stripe", "mui"], github: [{ linkFor: "Front-end", link: "https://github.com/LaylaSouthcombe/fashion-site-frontend"}], deploy: "https://main--rune-store.netlify.app/", img: rune}
     ]
 
     return (
@@ -30,7 +29,7 @@ const Projects = ({innerRef}) => {
                 </motion.div> 
                 <Project position="right" content={projects[0]}/> 
                 <Project position="left" content={projects[1]}/> 
-                <Project position="right" content={projects[2]}/> 
+                {/* <Project position="right" content={projects[2]}/>  */}
             </div>   
         </>
     )
